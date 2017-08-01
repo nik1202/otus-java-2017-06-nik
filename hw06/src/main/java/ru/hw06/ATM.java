7e560dc58de010a64b57673f75f6b66c2ecfd80d
nik <bespalov-ns@mail.ru>
2017-08-01 16:27:03 +0300

    ДЗ 06

---
+++ hw06/src/main/java/ru/hw06/ATM.java
@@ -0,0 +1,38 @@
+package ru.hw06;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class ATM {
+    private final Cell first;
+
+    public ATM(List<Cell> cells) {
+        Collections.sort(cells);
+        first = cells.get(0);
+        linkCells(cells);
+    }
+
+    public boolean withdraw(int requested) {
+        return first.withdraw(requested);
+    }
+
+    public int getBalance() {
+        Iterator<Cell> iterator = first.iterator();
+        int balance = 0;
+        while (iterator.hasNext()) {
+            balance += iterator.next().getBalance();
+        }
+        return balance;
+    }
+
+    private void linkCells(List<Cell> cells) {
+        Iterator<Cell> iterator = cells.iterator();
+        Cell cellA = iterator.next();
+        while (iterator.hasNext()) {
+            Cell cellB = iterator.next();
+            cellA.setNext(cellB);
+            cellA = cellB;
+        }
+    }
+}
