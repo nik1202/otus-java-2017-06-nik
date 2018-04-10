14fd95c90dcb8be78d90356731a7e283d877bbc0
nik <bespalov-ns@mail.ru>
2018-04-10 20:20:22 +0300

    TEST 05

--- hw02/src/test/java/ru/hw02/MainTest.java
+++ hw02/src/test/java/ru/hw02/MainTest.java
@@ -1,5 +1,6 @@
 package ru.hw02;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
@@ -10,6 +11,7 @@ import static org.junit.Assert.assertThat;
  *
  * Запускать с опцией -XX:-UseTLAB.
  */
+@Ignore
 public class MainTest {
     /**
      * Тест не пустой строки.
